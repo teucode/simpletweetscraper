1d61a2141bfdf9465858177855e2229b8e6fb026
tirzamp <62456713+teucode@users.noreply.github.com>
2022-07-27 07:39:40 +0700

    Add files via upload

---
+++ ambildataTw.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import csv
+import snscrape.modules.twitter as sntwitter
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import datetime as dt
+import time
+
+# Generating datetime objects
+from datetime import datetime, timedelta
+now = datetime.now()
+now = now.strftime('%Y-%m-%d')
+yesterday = datetime.now() - timedelta(days = 400)
+yesterday = yesterday.strftime('%Y-%m-%d')
+
+keyword = input('Enter a topic or keyword, please:')
+
+
+
+
+#Open/create a file to append data to
+csvFile = open(keyword +'-scrape-' + now + '.csv', 'a', newline='', encoding='utf8')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(['id','date','tweet',])
+
+
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper(keyword + ' lang:id since:' +  yesterday + ' -filter:links -filter:replies').get_items()):
+        csvWriter.writerow([tweet.id, tweet.date, tweet.content])
+csvFile.close()
